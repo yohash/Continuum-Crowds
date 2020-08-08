7c172159c505565e8d5104cf3c19a17b15afe747
jason-vreps <jason@vreps.us>
2020-08-08 23:10:42 +1200

    stop auto adding libraries when I make a class ref typo

--- Assets/Map Analyzer/TileGenerator.cs
+++ Assets/Map Analyzer/TileGenerator.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.EditorTools;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class TileGenerator : MonoBehaviour
 {
