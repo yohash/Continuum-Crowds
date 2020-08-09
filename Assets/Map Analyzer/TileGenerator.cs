26d0b7a4bb032a35cbe697758b0a7bfe92a6afd3
jheebl <jason@vreps.us>
2020-08-09 12:09:09 +1200

    Autoformat?

--- Assets/Map Analyzer/TileGenerator.cs
+++ Assets/Map Analyzer/TileGenerator.cs
@@ -4,251 +4,255 @@ using UnityEngine;
 
 public class TileGenerator : MonoBehaviour
 {
-	public string Filename = "Test";
-	public void SetFilename(string filename) { Filename = filename; }
+  public string Filename = "Test";
+  public void SetFilename(string filename) { Filename = filename; }
 
-	public int TileSize = 25;
+  public int TileSize = 25;
 
-	public List<MapTile> Tiles;
+  public List<MapTile> Tiles;
 
-	// terrain fields
-	private float[,] h;
-	private float[,] g;
-	private Vector2[,] dh;
+  public Vector2 DRAW_ME = Vector2.zero;
 
-	// viewables
-	private bool viewTiles;
-	private bool viewRegions;
-	private bool viewBorders;
-	private bool viewNeighborBorders;
-	public void ViewTiles(bool show) { viewTiles = show; }
-	public void ViewRegions(bool show) { viewRegions = show; }
-	public void ViewBorders(bool show) { viewBorders = show; }
-	public void ViewNeighbors(bool show) { viewNeighborBorders = show; }
-	public void ViewTileIndex(string i)
-	{
-		if (int.TryParse(i, out int index)) { tileIndex = index; }
-	}
-	public void ShuffleColors() { borderColors.Clear(); }
+  // terrain fields
+  private float[,] h;
+  private float[,] g;
+  private Vector2[,] dh;
 
-	private int tileIndex;
+  // viewables
+  private bool viewTiles;
+  private bool viewRegions;
+  private bool viewBorders;
+  private bool viewNeighborBorders;
+  public void ViewTiles(bool show) { viewTiles = show; }
+  public void ViewRegions(bool show) { viewRegions = show; }
+  public void ViewBorders(bool show) { viewBorders = show; }
+  public void ViewNeighbors(bool show) { viewNeighborBorders = show; }
+  public void ViewTileIndex(string i)
+  {
+    if (int.TryParse(i, out int index)) { tileIndex = index; }
+  }
+  public void ShuffleColors() { borderColors.Clear(); }
 
-	private List<Color> borderColors = new List<Color>();
+  private int tileIndex;
 
-	// ***************************************************************************
-	//  MONOBEHAVIOURS
-	// ***************************************************************************
-	private void Update()
-	{
-		float dy = 0.1f;
+  private List<Color> borderColors = new List<Color>();
 
-		if (viewTiles && Tiles.Count > tileIndex && tileIndex >= 0) {
-			var tile = Tiles[tileIndex];
+  // ***************************************************************************
+  //  MONOBEHAVIOURS
+  // ***************************************************************************
+  private void Update()
+  {
+    float dy = 0.1f;
 
-			float height(int x, int y) { return tile.Height[x - tile.Corner.x, y - tile.Corner.y] + dy; }
-			for (int x = tile.Corner.x; x < tile.Corner.x + tile.TileSize - 1; x++) {
-				for (int y = tile.Corner.y; y < tile.Corner.y + tile.TileSize - 1; y++) {
-					drawSquare(
-						new Vector3(x, height(x, y), y),
-						new Vector3(x + 1, height(x + 1, y), y),
-						new Vector3(x + 1, height(x + 1, y + 1), y + 1),
-						new Vector3(x, height(x, y + 1), y + 1),
-						Color.green
-					);
-				}
-			}
-		}
+    var hgt = Tiles.FirstOrDefault(t => t.ContainsPoint(DRAW_ME))?.Height[DRAW_ME.x, DRAW_ME.y];
 
-		if (viewRegions) {
-			foreach (var tile in Tiles) {
-				foreach (var region in tile.Regions) {
-					foreach (var location in region.Locations()) {
-						// handy call shortener
-						float height(Vector2Int v) { return tile.Height[v.x - tile.Corner.x, v.y - tile.Corner.y]; }
+    if (viewTiles && Tiles.Count > tileIndex && tileIndex >= 0) {
+      var tile = Tiles[tileIndex];
 
-						var hgt = location.ToXYZ(height(location) + dy);
-						drawSquare(hgt, Color.red);
-					}
-				}
-			}
-		}
+      float height(int x, int y) { return tile.Height[x - tile.Corner.x, y - tile.Corner.y] + dy; }
+      for (int x = tile.Corner.x; x < tile.Corner.x + tile.TileSize - 1; x++) {
+        for (int y = tile.Corner.y; y < tile.Corner.y + tile.TileSize - 1; y++) {
+          drawSquare(
+            new Vector3(x, height(x, y), y),
+            new Vector3(x + 1, height(x + 1, y), y),
+            new Vector3(x + 1, height(x + 1, y + 1), y + 1),
+            new Vector3(x, height(x, y + 1), y + 1),
+            Color.green
+          );
+        }
+      }
+    }
 
-		if (viewBorders) {
-			int i = 0;
-			foreach (var tile in Tiles) {
-				foreach (var border in tile.Borders) {
-					if (i + 1 > borderColors.Count) {
-						borderColors.Add(new Color(Random.value, Random.value, Random.value));
-					}
-					var c = borderColors[i++];
-					drawBorder(border, c);
-				}
-			}
-		}
+    if (viewRegions) {
+      foreach (var tile in Tiles) {
+        foreach (var region in tile.Regions) {
+          foreach (var location in region.Locations()) {
+            // handy call shortener
+            float height(Vector2Int v) { return tile.Height[v.x - tile.Corner.x, v.y - tile.Corner.y]; }
 
-		if (viewNeighborBorders) {
-			int i = 0;
-			var allBorders = new List<Border>();
-			foreach (var tile in Tiles) {
-				foreach (var border in tile.Borders) {
-					allBorders.Add(border);
-				}
-			}
+            var hgt = location.ToXYZ(height(location) + dy);
+            drawSquare(hgt, Color.red);
+          }
+        }
+      }
+    }
 
-			while (allBorders.Count > 0) {
-				if (i + 1 > borderColors.Count) {
-					borderColors.Add(new Color(Random.value, Random.value, Random.value));
-				}
-				var c = borderColors[i++];
-				var border = allBorders[0];
-				drawBorder(border, c);
-				foreach (var neighbor in border.GetNeighbors()) {
-					if (allBorders.Contains(neighbor)) {
-						// draw the neighbors
-						drawBorder(neighbor, c);
-						allBorders.Remove(neighbor);
-					}
-				}
-				allBorders.Remove(border);
-			}
-		}
-	}
+    if (viewBorders) {
+      int i = 0;
+      foreach (var tile in Tiles) {
+        foreach (var border in tile.Borders) {
+          if (i + 1 > borderColors.Count) {
+            borderColors.Add(new Color(Random.value, Random.value, Random.value));
+          }
+          var c = borderColors[i++];
+          drawBorder(border, c);
+        }
+      }
+    }
 
-	private void drawBorder(Border border, Color c)
-	{
-		float dy = 0.1f;
-		foreach (var location in border.GetLocations()) {
-			float height(Vector2Int v)
-			{
-				return border.Tile.Height[v.x - border.Tile.Corner.x, v.y - border.Tile.Corner.y];
-			}
-			var hgt = location.ToXYZ(height(location) + dy);
-			drawSquare(hgt, c);
-		}
-	}
+    if (viewNeighborBorders) {
+      int i = 0;
+      var allBorders = new List<Border>();
+      foreach (var tile in Tiles) {
+        foreach (var border in tile.Borders) {
+          allBorders.Add(border);
+        }
+      }
 
-	private void drawSquare(Vector3 corner, Color c)
-	{
-		Debug.DrawLine(corner, corner + Vector3.forward, c);
-		Debug.DrawLine(corner + Vector3.forward, corner + Vector3.forward + Vector3.right, c);
-		Debug.DrawLine(corner + Vector3.forward + Vector3.right, corner + Vector3.right, c);
-		Debug.DrawLine(corner + Vector3.right, corner, c);
-	}
+      while (allBorders.Count > 0) {
+        if (i + 1 > borderColors.Count) {
+          borderColors.Add(new Color(Random.value, Random.value, Random.value));
+        }
+        var c = borderColors[i++];
+        var border = allBorders[0];
+        drawBorder(border, c);
+        foreach (var neighbor in border.GetNeighbors()) {
+          if (allBorders.Contains(neighbor)) {
+            // draw the neighbors
+            drawBorder(neighbor, c);
+            allBorders.Remove(neighbor);
+          }
+        }
+        allBorders.Remove(border);
+      }
+    }
+  }
 
-	private void drawSquare(Vector3 c1, Vector3 c2, Vector3 c3, Vector3 c4, Color c)
-	{
-		Debug.DrawLine(c1, c2, c);
-		Debug.DrawLine(c2, c3, c);
-		Debug.DrawLine(c3, c4, c);
-		Debug.DrawLine(c4, c1, c);
-	}
+  private void drawBorder(Border border, Color c)
+  {
+    float dy = 0.1f;
+    foreach (var location in border.GetLocations()) {
+      float height(Vector2Int v)
+      {
+        return border.Tile.Height[v.x - border.Tile.Corner.x, v.y - border.Tile.Corner.y];
+      }
+      var hgt = location.ToXYZ(height(location) + dy);
+      drawSquare(hgt, c);
+    }
+  }
 
-	// ***************************************************************************
-	//  TILE GENERATION
-	// ***************************************************************************
-	public void GenerateCCTiles()
-	{
-		int sizeX = h.GetLength(0);
-		int sizeY = h.GetLength(1);
+  private void drawSquare(Vector3 corner, Color c)
+  {
+    Debug.DrawLine(corner, corner + Vector3.forward, c);
+    Debug.DrawLine(corner + Vector3.forward, corner + Vector3.forward + Vector3.right, c);
+    Debug.DrawLine(corner + Vector3.forward + Vector3.right, corner + Vector3.right, c);
+    Debug.DrawLine(corner + Vector3.right, corner, c);
+  }
 
-		if (h.GetLength(0) != g.GetLength(0) || g.GetLength(0) != dh.GetLength(0) ||
-				h.GetLength(1) != g.GetLength(1) || g.GetLength(1) != dh.GetLength(1)) {
-			Debug.LogError("Cannot generate Tiles, dimensions disagree. Re-load heightmaps.");
-			return;
-		}
+  private void drawSquare(Vector3 c1, Vector3 c2, Vector3 c3, Vector3 c4, Color c)
+  {
+    Debug.DrawLine(c1, c2, c);
+    Debug.DrawLine(c2, c3, c);
+    Debug.DrawLine(c3, c4, c);
+    Debug.DrawLine(c4, c1, c);
+  }
 
-		Debug.Log($"Generating tiles (size = {TileSize}) for height map dimension: {sizeX} x {sizeY}");
-		int numTilesX = Mathf.CeilToInt((float)sizeX / (float)TileSize);
-		int numTilesY = Mathf.CeilToInt((float)sizeY / (float)TileSize);
+  // ***************************************************************************
+  //  TILE GENERATION
+  // ***************************************************************************
+  public void GenerateCCTiles()
+  {
+    int sizeX = h.GetLength(0);
+    int sizeY = h.GetLength(1);
 
-		Debug.Log($"\t x-tiles ({numTilesX}), y-tiles ({numTilesY}), total ({numTilesX * numTilesY})");
-		Tiles = new List<MapTile>();
-		for (int x = 0; x < numTilesX; x++) {
-			for (int y = 0; y < numTilesY; y++) {
-				Vector2Int corner = new Vector2Int(x * TileSize, y * TileSize);
-				Tiles.Add(new MapTile(
-							corner,
-							h.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize),
-							g.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize),
-							dh.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize)
-				));
-			}
-		}
+    if (h.GetLength(0) != g.GetLength(0) || g.GetLength(0) != dh.GetLength(0) ||
+        h.GetLength(1) != g.GetLength(1) || g.GetLength(1) != dh.GetLength(1)) {
+      Debug.LogError("Cannot generate Tiles, dimensions disagree. Re-load heightmaps.");
+      return;
+    }
 
-		Debug.Log($"\tPairing up tiles with neighbors...");
-		foreach (var tile in Tiles) {
-			foreach (var neighbor in Tiles) {
-				if (tile != neighbor) {
-					if ((tile.Corner - neighbor.Corner).magnitude == TileSize) {
-						// this tile is within one TileSize, it is a neighbor
-						var dir = (neighbor.Corner - tile.Corner).ToDirection();
-						tile.NeighborTiles[dir] = neighbor;
-					} else if (
-							Mathf.Abs(tile.Corner.x - neighbor.Corner.x) == TileSize &&
-							Mathf.Abs(tile.Corner.y - neighbor.Corner.y) == TileSize
-					) {
-						// this tile is a diagonal neighbor
-						//tile.DiagonalNeighbors.Add(neighbor);
-					}
-				}
-			}
-		}
+    Debug.Log($"Generating tiles (size = {TileSize}) for height map dimension: {sizeX} x {sizeY}");
+    int numTilesX = Mathf.CeilToInt((float)sizeX / (float)TileSize);
+    int numTilesY = Mathf.CeilToInt((float)sizeY / (float)TileSize);
 
-		// now that tiles are generated, build all connections between tiles
-		Debug.Log($"\tPairing up borders with neighbors...");
-		foreach (var tile in Tiles) {
-			Debug.Log($"\t\tAssembling interconnects for tile {tile.Corner}...");
-			tile.AssembleInterconnects();
-		}
+    Debug.Log($"\t x-tiles ({numTilesX}), y-tiles ({numTilesY}), total ({numTilesX * numTilesY})");
+    Tiles = new List<MapTile>();
+    for (int x = 0; x < numTilesX; x++) {
+      for (int y = 0; y < numTilesY; y++) {
+        Vector2Int corner = new Vector2Int(x * TileSize, y * TileSize);
+        Tiles.Add(new MapTile(
+              corner,
+              h.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize),
+              g.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize),
+              dh.SubMatrix(x * TileSize, y * TileSize, TileSize, TileSize)
+        ));
+      }
+    }
 
-		Debug.Log("Final accouting:");
-		foreach (var tile in Tiles) {
-			Debug.Log("\tTile " + tile.Corner + ": " + tile.Borders.Count + " borders");
-			foreach (var dir in Directions.Each()) {
-				var matching = tile.Borders.Where(b => b.Direction == dir);
-				if (matching.Count() > 0) { Debug.Log($"\t\t\t" + dir); }
-				int num = 1;
-				foreach (var card in matching) {
-					Debug.Log($"\t\t\t\t({num++}) - {card.Average}:  " + string.Join(", ", card.GetLocations()));
-					int n = 1;
-					foreach (var neighb in card.GetNeighbors()) {
-						Debug.Log($"\t\t\t\t\t(Neighbor {n++}) - {neighb.Average}");
-					}
-				}
-			}
-		}
-	}
+    Debug.Log($"\tPairing up tiles with neighbors...");
+    foreach (var tile in Tiles) {
+      foreach (var neighbor in Tiles) {
+        if (tile != neighbor) {
+          if ((tile.Corner - neighbor.Corner).magnitude == TileSize) {
+            // this tile is within one TileSize, it is a neighbor
+            var dir = (neighbor.Corner - tile.Corner).ToDirection();
+            tile.NeighborTiles[dir] = neighbor;
+          } else if (
+              Mathf.Abs(tile.Corner.x - neighbor.Corner.x) == TileSize &&
+              Mathf.Abs(tile.Corner.y - neighbor.Corner.y) == TileSize
+          ) {
+            // this tile is a diagonal neighbor
+            //tile.DiagonalNeighbors.Add(neighbor);
+          }
+        }
+      }
+    }
 
-	// ***************************************************************************
-	//  FILE IO
-	// ***************************************************************************
-	public void LoadCsvFiles()
-	{
-		string path = $"{FileUtility.PATH}/{FileUtility.DATA_FOLDER}/{Filename}/{FileUtility.CSV_FOLDER}/";
-		Debug.Log("Loading files at: " + path + Filename + "_*");
+    // now that tiles are generated, build all connections between tiles
+    Debug.Log($"\tPairing up borders with neighbors...");
+    foreach (var tile in Tiles) {
+      Debug.Log($"Assembling interconnects for tile {tile.Corner}...");
+      tile.AssembleInterconnects();
+    }
 
-		h = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_H.txt");
-		Debug.Log($"\tSuccessfully loaded height map, h: {h.GetLength(0)}x{h.GetLength(1)}");
+    Debug.Log("Final accouting:");
+    foreach (var tile in Tiles) {
+      Debug.Log("\tTile " + tile.Corner + ": " + tile.Borders.Count + " borders");
+      foreach (var dir in Directions.Each()) {
+        var matching = tile.Borders.Where(b => b.Direction == dir);
+        if (matching.Count() > 0) { Debug.Log($"\t\t" + dir); }
+        int num = 1;
+        foreach (var card in matching) {
+          Debug.Log($"\t\t\t({num++}) - [{card.ID}]:\t\t" + string.Join(", ", card.GetLocations()));
+          int n = 1;
+          foreach (var neighb in card.GetNeighbors()) {
+            Debug.Log($"\t\t\t\t(Neighbor {n++}) - [{neighb.ID}]");
+          }
+        }
+      }
+    }
+  }
 
-		g = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_g.txt");
-		Debug.Log($"\tSuccessfully loaded discomfort, g: {g.GetLength(0)}x{g.GetLength(1)}");
+  // ***************************************************************************
+  //  FILE IO
+  // ***************************************************************************
+  public void LoadCsvFiles()
+  {
+    string path = $"{FileUtility.PATH}/{FileUtility.DATA_FOLDER}/{Filename}/{FileUtility.CSV_FOLDER}/";
+    Debug.Log("Loading files at: " + path + Filename + "_*");
 
-		float[,] dhdx = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_dHdx.txt");
-		float[,] dhdy = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_dHdy.txt");
+    h = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_H.txt");
+    Debug.Log($"\tSuccessfully loaded height map, h: {h.GetLength(0)}x{h.GetLength(1)}");
 
-		// populate matrix dh
-		dh = new Vector2[dhdx.GetLength(0), dhdx.GetLength(1)];
-		for (int i = 0; i < dhdx.GetLength(0); i++) {
-			for (int k = 0; k < dhdx.GetLength(1); k++) {
-				dh[i, k] = new Vector2(dhdx[i, k], dhdy[i, k]);
-			}
-		}
+    g = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_g.txt");
+    Debug.Log($"\tSuccessfully loaded discomfort, g: {g.GetLength(0)}x{g.GetLength(1)}");
 
-		Debug.Log($"\tSuccessfully assembled height gradient, dh: {dh.GetLength(0)}x{dh.GetLength(1)}");
-	}
+    float[,] dhdx = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_dHdx.txt");
+    float[,] dhdy = FileUtility.LoadCsvIntoFloatMatrix(path + Filename + "_dHdy.txt");
 
-	public void SerializeAndSaveTiles()
-	{
-		/// TBD
-	}
+    // populate matrix dh
+    dh = new Vector2[dhdx.GetLength(0), dhdx.GetLength(1)];
+    for (int i = 0; i < dhdx.GetLength(0); i++) {
+      for (int k = 0; k < dhdx.GetLength(1); k++) {
+        dh[i, k] = new Vector2(dhdx[i, k], dhdy[i, k]);
+      }
+    }
+
+    Debug.Log($"\tSuccessfully assembled height gradient, dh: {dh.GetLength(0)}x{dh.GetLength(1)}");
+  }
+
+  public void SerializeAndSaveTiles()
+  {
+    /// TBD
+  }
 }
