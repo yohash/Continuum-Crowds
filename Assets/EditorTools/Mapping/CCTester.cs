eebed1b22786a56e3bc83fe2aa32913de4f6f4a1
jheebl <jason@vreps.us>
2021-01-18 09:43:26 +1300

    revert change made to interolpolations.cs in error

--- Assets/EditorTools/Mapping/CCTester.cs
+++ Assets/EditorTools/Mapping/CCTester.cs
@@ -148,9 +148,6 @@ public class CCTester : MonoBehaviour
         var height = currentTile.Height(worldX, worldY);
         var start = new Vector3(worldX - vel.x / 2, height, worldY - vel.y / 2);
         var end = new Vector3(worldX + vel.x / 2, height, worldY + vel.y / 2);
-
-        UnityEngine.Debug.Log("drawing vel (" + vel + "): " + start.x + " to " + end.x);
-
         UnityEngine.Debug.DrawLine(start, end, Color.blue);
       }
     }
