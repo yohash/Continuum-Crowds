ccf5bb75141e179d03580294458ced296e59d38f
jason-vreps <jason@vreps.us>
2021-01-25 21:42:24 +1300

    after some research on my previous methods, some groundwork was laid for the CC_Unit footprint computations

--- Assets/Continuum Crowds/Classes/CC_Unit.cs
+++ Assets/Continuum Crowds/Classes/CC_Unit.cs
@@ -4,6 +4,9 @@ using System;
 [System.Serializable]
 public class CC_Unit
 {
+  // class vars
+  private static readonly float fadeout = 2.5f;
+
   // store unit size
   public int sizeX;
   public int sizeY;
@@ -16,21 +19,33 @@ public class CC_Unit
   private Func<Vector2> getRotation;
   private Func<Vector2> getPosition;
 
+  // local vars
+  private float[,] baseprint;
+
   public CC_Unit(Func<Vector2> getVelocity, Func<Vector2> getRotation, Func<Vector2> getPosition, Vector2 unitDimensions)
   {
     sizeX = (int)Math.Round(unitDimensions.x);
     sizeY = (int)Math.Round(unitDimensions.y);
 
+    if (sizeX < 1 || sizeY < 1) {
+      Debug.LogWarning("CC_Unit created with unit size dimension <1");
+    }
+
+    // ensure we're clamped above 1
+    sizeX = Math.Max(sizeX, 1);
+    sizeY = Math.Max(sizeY, 1);
+
     this.getVelocity = getVelocity;
     this.getRotation = getRotation;
     this.getPosition = getPosition;
+
+    baseprint = computeBaseFootprint();
   }
 
   public float[,] GetFootprint()
   {
     // use getPosition(), getRotation(), and unit size to compute
     // the footprint, properly interpolated and splatted into a 2x2 grid
-
 
     // wrap getPostion to the range 0 < position < 1 (grid size)
     float xOffset = getPosition().x.Modulus(1f);
@@ -40,6 +55,27 @@ public class CC_Unit
 
     // TODO: figure out, based on sizeX and sizeY, and rotation, how big footprint should be
     float[,] footprint = new float[sizeX, sizeY];
+
+    return footprint;
+  }
+
+  private float[,] computeBaseFootprint()
+  {
+    // (1) initialize 'positions' with the standard grid and dimensions provided 
+    int buffer = (int)Math.Ceiling(fadeout);
+    int cols = sizeX + buffer * 2;
+    int rows = sizeY + buffer * 2;
+
+    Vector2 center = new Vector2((cols - 1) / 2f, (rows - 1) / 2f);
+
+    // init the footprint
+    var footprint = new float[cols, rows];
+
+    for (int x = 0; x < cols; x++) {
+      for (int y = 0; y < rows; y++) {
+        // first, see if x is outside the footprint zone
+      }
+    }
 
     return footprint;
   }
